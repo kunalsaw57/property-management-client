c05d462a46be28d571473f18ab265584595aa30f
Kunal Pathak <kunalsaw57@gmail.com>
2023-03-04 11:44:14 +0530

    Minor updates

--- src/components/common/PropertyCard.tsx
+++ src/components/common/PropertyCard.tsx
@@ -7,15 +7,14 @@ const PropertyCard = ({ id, title, location, price, photo }: PropertyCardProps) 
   return (
     <Card
       component={Link}
-      to={`/properties/${id}`}
+      to={`/properties/show/${id}`}
       sx={{
         maxWidth: '330px',
         padding: '10px',
         '&:hover': {
           boxShadow: '0 22px 45px rgba(176,176,176,0.1)'
         },
-        cursor: 'pointer',
-        textDecoration: 'none'
+        cursor: 'pointer'
       }}
       elevation={0}
     >
