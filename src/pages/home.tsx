6c6fe17251a230bf885a8fb544ce3b068b1c6889
Kunal Pathak <kunalsaw57@gmail.com>
2023-03-12 10:28:04 +0530

    Done all pages

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -9,7 +9,21 @@ import PropertyCard from '../components/common/PropertyCard'
 import TopAgent from '../components/home/TopAgent'
 
 
-const home = () => {
+const Home = () => {
+    const { data, isLoading, isError } = useList({
+        resource: 'properties',
+        config: {
+            pagination: {
+                pageSize: 3
+            }
+        }
+    })
+
+    const latestProperties = data?.data??[];
+
+    if(isLoading) return <div>Loading...</div> 
+    if(isError) return <div>Error</div> 
+
     return (
         <Box>
             <Typography fontSize={25} fontWeight={700} color="#11142D">
@@ -45,8 +59,32 @@ const home = () => {
                 <TotalRevenue />
                 <PropertyReferrals />
             </Stack>
+            <Box
+                flex={1}
+                borderRadius="15px"
+                padding="20px"
+                bgcolor="#fcfcfc"
+                display="flex"
+                flexDirection="column"
+                minWidth="100%"
+                mt="25px"
+            >
+                <Typography fontSize="18px" fontWeight={600} color="#11142d">Latest Properties</Typography>
+                <Box mt={2.5} sx={{ display: 'flex', flexWrap: 'wrap', gap: 4 }} >
+                    {latestProperties.map((property)=>(
+                        <PropertyCard
+                            key={property._id}
+                            id={property._id}
+                            title={property.title}
+                            location={property.location}
+                            price={property.price}
+                            photo={property.photo}
+                        />
+                    ))}
+                </Box>
+            </Box>
         </Box>
     )
 }
 
-export default home
+export default Home
